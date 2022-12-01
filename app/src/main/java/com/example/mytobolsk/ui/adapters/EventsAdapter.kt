3ce0bfc909a05d25810a6190c5d132d6ae91406a
Egor Dubina <belochka20022@gmail.com>
2022-12-01 08:19:37 +0500

    added onclick on item in recyclerview, added animation for recyclerview

--- app/src/main/java/com/example/mytobolsk/ui/adapters/EventsAdapter.kt
+++ app/src/main/java/com/example/mytobolsk/ui/adapters/EventsAdapter.kt
@@ -7,24 +7,27 @@ import com.example.mytobolsk.R
 import com.example.mytobolsk.databinding.ItemEventCardBinding
 import com.example.mytobolsk.ui.models.Event
 
-class EventsAdapter(private val eventsDataset: List<Event>) :
-    RecyclerView.Adapter<EventsAdapter.ViewHolder>() {
+class EventsAdapter(
+    private val eventsDataset: List<Event>,
+    private val clickListener: (Event) -> Unit
+) : RecyclerView.Adapter<EventsAdapter.ViewHolder>() {
 
     inner class ViewHolder(val binding: ItemEventCardBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemEventCardBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemEventCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder) {
+            itemView.setOnClickListener { clickListener(eventsDataset[position]) }
+
             with(eventsDataset[position]) {
                 binding.itemEventCardTitle.text = title
-                binding.itemEventCardPlace.text =
-                    holder.itemView.context.getString(R.string.`where`, place)
+                binding.itemEventCardPlace.text = holder.itemView.context.getString(R.string.`where`, place)
                 binding.itemEventCardTime.text =
                     holder.itemView.context.getString(R.string.`when`, date, time)
             }
--- app/src/main/java/com/example/mytobolsk/ui/adapters/StoriesAdapter.kt
+++ app/src/main/java/com/example/mytobolsk/ui/adapters/StoriesAdapter.kt
@@ -4,25 +4,28 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mytobolsk.databinding.ItemStoryItemBinding
-import com.example.mytobolsk.ui.models.Route
 import com.example.mytobolsk.ui.models.Story
 
-class StoriesAdapter(private val storyDataSet: List<Story>) :
-    RecyclerView.Adapter<StoriesAdapter.ViewHolder>() {
+class StoriesAdapter(
+    private val storyDataSet: List<Story>, private val clickListener: (Story) -> Unit
+) : RecyclerView.Adapter<StoriesAdapter.ViewHolder>() {
 
     inner class ViewHolder(val binding: ItemStoryItemBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemStoryItemBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemStoryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder) {
-            with(storyDataSet[position]) {
-                binding.itemStoryTitle.text = title
+            itemView.setOnClickListener { clickListener(storyDataSet[position]) }
+            with(binding) {
+                with(storyDataSet[position]) {
+                    itemStoryTitle.text = title
+                }
             }
         }
     }
